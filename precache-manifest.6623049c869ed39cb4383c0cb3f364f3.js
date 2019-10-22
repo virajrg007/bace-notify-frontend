f149b508fb504c28d37c6e54d358efa247916e2c
VirajRG <viraj.gawde123@gmail.com>
2019-10-22 17:17:13 +0530

    Updates

---
+++ precache-manifest.6623049c869ed39cb4383c0cb3f364f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cbbb4fe0fa008e68bd27e05b12f9ba0",
+    "url": "/bace-notify-frontend/index.html"
+  },
+  {
+    "revision": "b3bda8ee4c2ad3b4c6c5",
+    "url": "/bace-notify-frontend/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "117573ec6e7b4c945e2a",
+    "url": "/bace-notify-frontend/static/js/2.ae4855e4.chunk.js"
+  },
+  {
+    "revision": "b3bda8ee4c2ad3b4c6c5",
+    "url": "/bace-notify-frontend/static/js/main.f3b141fa.chunk.js"
+  },
+  {
+    "revision": "d20a12da2857fd37bfad",
+    "url": "/bace-notify-frontend/static/js/runtime-main.a6632537.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/bace-notify-frontend/static/media/logo.25bf045c.svg"
+  }
+]);
